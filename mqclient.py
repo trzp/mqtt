1224a8be353e59b33758049f509c520d7c04d644
trzp <mrtang@nudt.edu.cn>
2020-02-01 23:24:32 +0800

    ok

--- mqclient.py
+++ mqclient.py
@@ -21,17 +21,17 @@ connect_rc = {'0':'[mqtt] connect succeed!',
 
 class MqClient():
     def __init__(self,username = '', client_id = '', host = '127.0.0.1',port = 1883, sub_topic = '', keepalive = 600, loop_sync = True):
-		# username: 	用户名，为空时缺省
-		# client_id: 	客户端标识符，为空时系统自动分配
-		# host: 		代理的地址
-		# port:			代理的端口
-		# sub_topic:	订阅的主题，为空时不订阅任何主题
-		# keepalive:	与代理连接的时间
-		# loop_sync:	True: 以loop_start方式，启用线程来处理网络请求事件，主线程可以对发布以及订阅作进一步处理，常用
-		#				False:以loop_forever方式，阻塞。
+        # username:     用户名，为空时缺省
+        # client_id:     客户端标识符，为空时系统自动分配
+        # host:         代理的地址
+        # port:            代理的端口
+        # sub_topic:    订阅的主题，为空时不订阅任何主题
+        # keepalive:    与代理连接的时间
+        # loop_sync:    True: 以loop_start方式，启用线程来处理网络请求事件，主线程可以对发布以及订阅作进一步处理，常用
+        #                False:以loop_forever方式，阻塞。
         
-		self.sub_topic = sub_topic
-        self.client = mqtt.Client(client_id)
+        self.sub_topic = sub_topic
+        self.client = mqtt.Client(client_id,)
         if len(username) > 0:
             self.client.username_pw_set(username,password = None)
         self.client.on_connect = self.on_connect
@@ -47,14 +47,14 @@ class MqClient():
         self.client.loop_stop()
 
     def on_connect(self,client,userdata,flags,rc):
+        if rc == 0 and len(self.sub_topic) > 0:
+            client.subscribe(self.sub_topic)
+
         rc = str(rc)
         if rc in connect_rc:
             print(connect_rc[rc])
         else:
             print('[mqtt] connect failure')
-
-        if rc == 0 and len(self.sub_topic) > 0:
-            client.subscribe(self.sub_topic)
 
     def on_message(self,client,userdata,msg):
         mg = msg.payload
@@ -67,13 +67,21 @@ class MqClient():
 
     def disconnect(self):
         self.client.disconnect()
+        
+def main():
+    mq = MqClient(host = '192.168.66.2',sub_topic = 'esp32topic',loop_sync = False)
 
-if __name__ == '__main__':
+
+def main2():
     import time
-    mqp = MqClient(client_id = 'pub_0')
+    mqp = MqClient(host = '192.168.66.2',client_id = 'pub_0')
     for i in range(10):
         mqp.publish(str(i),'test')
         time.sleep(1)
         print(str(i))
+
+if __name__ == '__main__':
+    main()
+    
 
 
